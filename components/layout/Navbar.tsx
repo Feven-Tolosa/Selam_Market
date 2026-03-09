516471827a9de023a8ef22e7264214af30e280a0
Feven-Tolosa <tolosafeven0@gmail.com>
2026-03-09 17:21:24 +0300

    style change

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -4,17 +4,28 @@ import Image from 'next/image'
 
 export default function Navbar() {
   return (
-    <header className='bg-[#10b5cb] text-white shadow-md'>
+    <header className='bg-[#b6f0fa] text-gray-700 shadow-md'>
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
         <div className='flex justify-between h-16 items-center'>
           {/* Logo */}
-          <div className='flex-shrink-0'>
-            <Image
-              src='/logo.png' // replace with your logo
-              alt='Logo'
-              width={120}
-              height={40}
-            />
+          <div className='flex-shrink-0 pt-3'>
+            <Image src='/logo.png' alt='Logo' width={120} height={40} />
+          </div>
+          {/* Language selector */}
+          <div className='flex items-center space-x-2'>
+            <span className='text-sm'>EN</span>
+            <svg
+              className='w-4 h-4 text-gray-700'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 20 20'
+              fill='currentColor'
+            >
+              <path
+                fillRule='evenodd'
+                d='M5.23 7.21a.75.75 0 011.06.02L10 11.584l3.71-4.354a.75.75 0 111.14.976l-4 4.708a.75.75 0 01-1.14 0l-4-4.708a.75.75 0 01.02-1.06z'
+                clipRule='evenodd'
+              />
+            </svg>
           </div>
 
           {/* Search bar */}
@@ -32,7 +43,7 @@ export default function Navbar() {
           <div className='flex items-center space-x-4'>
             {/* Categories menu */}
             <div className='relative group'>
-              <button className='flex items-center space-x-1 hover:text-gray-200'>
+              <button className='flex items-center space-x-1 hover:text-gray-500'>
                 <Menu size={20} />
                 <span className='hidden sm:inline'>Categories</span>
               </button>
@@ -52,7 +63,7 @@ export default function Navbar() {
             </div>
 
             {/* Cart icon */}
-            <button className='relative hover:text-gray-200'>
+            <button className='relative hover:text-gray-500'>
               <ShoppingCart size={24} />
               <span className='absolute -top-1 -right-2 bg-red-500 text-white rounded-full text-xs w-5 h-5 flex items-center justify-center'>
                 3
@@ -60,7 +71,7 @@ export default function Navbar() {
             </button>
 
             {/* User icon */}
-            <button className='hover:text-gray-200'>
+            <button className='hover:text-gray-500'>
               <User size={24} />
             </button>
           </div>
